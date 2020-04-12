97ea543df8d509dd0e332c359f28086a9fd1713e
Yurii Krupenik <45948675+totozao@users.noreply.github.com>
2020-04-12 18:39:37 +0300

    Nudes V3.0
    Signed-off-by: Totozao <totozao.totozao@yandex.ru>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,30 @@
 <template>
-    <v-container fluid fill-height class="home-hero" style="max-height:100vh;">
-        <v-layout justify-center align-center column pa-5>
-            <div class="display-4 font-weight-black black--text text-xs-center">Хуя у меня два</div>
-            <div class="display-4 font-weight-black black--text text-xs-center mb-3">и</div>
-            <div class="display-1 font-weight-bold black--text text-xs-center"> меча два.</div>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height class="home-hero" style="max-height:100vh;">
+    <v-row justify-center pa-5 color="indigo--text text--center">
+      <v-col xs="12" lg="7">
+        <div class="display-4 font-weight-bold text-xs-center homeText">Хуя у меня два</div>
+        <div class="display-4 font-weight-bold text-xs-center mb-3 text--center homeText">и</div>
+        <div class="display-1 font-weight-bold text-xs-center homeText">меча два.</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 
 <script>
 export default {
-    name: 'HomeHero'
+  name: "Home"
 };
 </script>
 
 <style scoped>
-.home-hero {
-    background: url('https://fbcd.co/images/products/dbf9baed1cf869fde664a1e75bad2db5_resize.jpg');
+  .home-hero {
+    background: url("https://fbcd.co/images/products/dbf9baed1cf869fde664a1e75bad2db5_resize.jpg");
     background-size: cover;
     width: 100%;
     height: 100%;
-}
+  }
+  .homeText {
+    text-align: center;
+  }
 </style>
