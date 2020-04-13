f255fa92093702c5dd130d363f6f6981a5736dce
Devaiser <63445437+Devaiser@users.noreply.github.com>
2020-04-13 17:18:27 +0300

    Добавили иерархию папок и роуты двух курсов

--- src/views/Courses/1-course.vue
+++ src/views/Courses/1-course.vue
@@ -20,7 +20,7 @@
                     <span>Выберите группу</span>
                   </v-btn>
                 </template>
-                <v-list rounded>
+                <v-list>
                   <v-list-item @click="''" v-for="gr of faculty.groups" :key="gr.group" router :to="gr.url">
                     <v-list-item-title class="text-center list">
                       {{ gr.group }}
@@ -44,21 +44,28 @@ export default {
       faculties: [
         { name: 'Бизнес-информатика', icon:'mdi-laptop',
           groups: [
-            { group: 'БИ-181', url: '/course_1/bi_181'}
+            { group: 'БИ-б-о-191', url: '/course_1/bi_191'}
         ]
         },
         { name: 'Торговое дело', icon: 'mdi-cash-register',
           groups: [
-            { group: 'ТД-181', url: '/course_1/td_181'},
-            { group: 'ТД-182', url: '/course_1/td_182'}
+            { group: 'ТД-б-о-191', url: '/course_1/td_191'},
           ],
-
         }, 
-        { name: 'Экономика', icon: 'mdi-finance',},
-        { name: 'Менеджмент', icon: 'mdi-account-group', },
-        { name: 'Гос. муниципальное управление', icon: 'mdi-account-tie', },
-        { name: 'Управление персоналом', icon: 'mdi-account-group', },
-        { name: 'Финансы и кредит', icon: 'mdi-credit-card-outline', }
+        { name: 'Экономика', icon: 'mdi-finance',
+          groups: [
+            { group: 'Э-б-о-191', url: '/course_1/e_191'},
+            { group: 'Э-б-о-192', url: '/course_1/e_192'},
+            { group: 'Э-б-о-193', url: '/course_1/e_193'},
+            { group: 'Э-б-о-194', url: '/course_1/e_194'},
+          ]  
+        },
+        { name: 'Гос. муниципальное управление', icon: 'mdi-account-tie', 
+          groups: [
+            { group: 'ГМУ-б-о-191', url: '/course_1/gu_191'},
+            { group: 'ГМУ-б-о-192', url: '/course_1/gu_192'},
+          ]
+        },
       ],
     }
   }
@@ -71,7 +78,7 @@ h1 {
   font-family: Calibri;
   margin-top: 2%
 }
-item {
+.item {
   text-align: center;
 }
 .list {
