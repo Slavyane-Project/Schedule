a4a8bec6dd5028f7c40dda355112ad0343d628d9
Devaiser <63445437+Devaiser@users.noreply.github.com>
2020-04-14 17:56:39 +0300

    Добавили страницу расписание

--- src/views/Direction/Business-informatics/2_course/bi-181.vue
+++ src/views/Direction/Business-informatics/2_course/bi-181.vue
@@ -1,80 +1,179 @@
 <template>
-  <div>
-    <v-container class="mt-5">
-      <v-row class="justify-center">
-        <v-col class="justify-center" cols="12" sm="6" md="4" lg="4" v-for="day of days" :key="day.name">
-          <v-card shaped flat elevation="3" class="text-center ma-3">
-            <v-list-item v-for="day of days" :key="day.name">
-              {{ day.name }}
-            </v-list-item>
-            <v-list-item two-line v-for="lesson of day" :key="lesson.number">
-              <v-list-item-content>
-                <div class="overline mb-4">{{ lesson.name }}</div>
-                <v-list-item-title class="headline mb-1">{{ lesson.teacher }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-text class="pb-6">
-              <div class="title black--text">{{ lesson.time }}</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-
-    </v-container>
-  </div>
+    <Schedule
+      :days="days"
+    />
 </template>
 
 <script>
+import Schedule from '@/components/Schedule.vue'
+
 export default {
-data () {
-  return {
-    days: [
-      {name: "Понедельник",
-        lessons: [
-          {number: '1', time: "09:50-11:20", name: 'ЖЦИС', teacher: 'Королёв О.Л.', link: ''},
-          {number: '2', time: "11:30-13:00", name: 'Культурология', teacher: 'Кравченко И.С.', link: ''},
-          {number: '3', time: "13:20-14:50", name: 'БЖД', teacher: 'Предпопие', link: ''}
-        ]
-      },
-      
-      {name: "Вторник",
-        lessons: [
-          {number: '1', time: "09:50-11:20", name: 'ЖЦИС', teacher: 'Королёв О.Л.', link: ''},
-          {number: '2', time: "11:30-13:00", name: 'Культурология', teacher: 'Кравченко И.С.', link: ''},
-          {number: '3', time: "13:20-14:50", name: 'БЖД', teacher: 'Предпопие', link: ''}
-        ]
-      },
+  data() {
+    return {
+      days: [
+        {
+          name: "Понедельник",
+          lessons: [
+            {
+              number: "1",
+              time: "09:50-11:20",
+              nameCh: "ЖЦИС",
+              teacherCh: "Королёв О.Л.",
+              auditoryCh: '306',
+              link: "",
+              nameZ: 'ППИ',
+              teacherZ: 'Королёв О.Л.',
+              auditoryZ: '307'
+            },
+            {
+              number: "2",
+              time: "11:30-13:00",
+              name: "Культурология",
+              teacher: "Кравченко И.С.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "3",
+              time: "13:20-14:50",
+              name: "БЖД",
+              teacher: "Предпопие",
+              auditory: '306',
+              link: ""
+            }
+          ]
+        },
 
-      {name: "Среда",
-        lessons: [
-          {number: '1', time: "09:50-11:20", name: 'ЖЦИС', teacher: 'Королёв О.Л.', link: ''},
-          {number: '2', time: "11:30-13:00", name: 'Культурология', teacher: 'Кравченко И.С.', link: ''},
-          {number: '3', time: "13:20-14:50", name: 'БЖД', teacher: 'Предпопие', link: ''}
-        ]
-      },
+        {
+          name: "Вторник",
+          lessons: [
+            {
+              number: "1",
+              time: "09:50-11:20",
+              name: "ЖЦИС",
+              teacher: "Бойченко",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "2",
+              time: "11:30-13:00",
+              name: "Культурология",
+              teacher: "Кравченко И.С.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "3",
+              time: "13:20-14:50",
+              name: "БЖД",
+              teacher: "Предпопие",
+              auditory: '306',
+              link: ""
+            }
+          ]
+        },
 
-      {name: "Четверг",
-        lessons: [
-          {number: '1', time: "09:50-11:20", name: 'ЖЦИС', teacher: 'Королёв О.Л.', link: ''},
-          {number: '2', time: "11:30-13:00", name: 'Культурология', teacher: 'Кравченко И.С.', link: ''},
-          {number: '3', time: "13:20-14:50", name: 'БЖД', teacher: 'Предпопие', link: ''}
-        ]
-      },
+        {
+          name: "Среда",
+          lessons: [
+            {
+              number: "2",
+              time: "09:50-11:20",
+              name: "ЖЦИС",
+              teacher: "Королёв О.Л.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "2",
+              time: "11:30-13:00",
+              name: "Культурология",
+              teacher: "Кравченко И.С.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "3",
+              time: "13:20-14:50",
+              name: "БЖД",
+              teacher: "Предпопие",
+              auditory: '306',
+              link: ""
+            }
+          ]
+        },
 
-      {name: "Пятница",
-        lessons: [
-          {number: '1', time: "09:50-11:20", name: 'ЖЦИС', teacher: 'Королёв О.Л.', link: ''},
-          {number: '2', time: "11:30-13:00", name: 'Культурология', teacher: 'Кравченко И.С.', link: ''},
-          {number: '3', time: "13:20-14:50", name: 'БЖД', teacher: 'Предпопие', link: ''}
-        ]
-      }
+        {
+          name: "Четверг",
+          lessons: [
+            {
+              number: "1",
+              time: "09:50-11:20",
+              name: "ЖЦИС",
+              teacher: "Королёв О.Л.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "2",
+              time: "11:30-13:00",
+              name: "Культурология",
+              teacher: "Кравченко И.С.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "3",
+              time: "13:20-14:50",
+              name: "БЖД",
+              teacher: "Предпопие",
+              auditory: '306',
+              link: ""
+            }
+          ]
+        },
 
-    ]
+        {
+          name: "Пятница",
+          lessons: [
+            {
+              number: "1",
+              time: "09:50-11:20",
+              name: "ЖЦИС",
+              teacher: "Королёв О.Л.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "2",
+              time: "11:30-13:00",
+              name: "Культурология",
+              teacher: "Кравченко И.С.",
+              auditory: '306',
+              link: ""
+            },
+            {
+              number: "3",
+              time: "13:20-14:50",
+              name: "БЖД",
+              teacher: "Предпопие",
+              auditory: '306',
+              link: ""
+            }
+          ]
+        }
+      ]
+    }
+  },
+  components: {
+    Schedule
   }
 }
-}
 </script>
-
-<style>
+<style scoped>
 
 </style>
+
+
+
