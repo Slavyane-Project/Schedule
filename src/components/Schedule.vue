ddea55118f826f880c1fa93bc9e53ffaadb91f50
Totozao <totozao.totozao@yandex.ru>
2020-04-14 22:55:03 +0300

    Fix devices (but not all)
    ГИГИ

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -2,29 +2,39 @@
   <div>
     <v-container class="mt-5">
       <v-row class="justify-center">
-        <v-col md="4" lg="4" class="justify-center mb-6" v-for="day of days" :key="day.name" flat >
+        <v-col cols="11" md="12" lg="9" class="justify-center mb-6" v-for="day of days" :key="day.name" flat >
           <v-card>
             <v-card-title class="justify-center">{{ day.name }}</v-card-title>
-            <v-card v-for="lesson of day.lessons" :key="lesson.number" class="my-1">
+            <v-card v-for="lesson of day.lessons" :key="lesson.number" class="my-3" elevation="4">
               <v-row> 
-                  <v-col cols="6" sm="4"  md="3">
-                    <div class="caption grey--text ml-7">Время:</div>
-                    <div class="ml-2 pt-2">{{ lesson.time }}</div>
+                  <v-col cols="5" sm="2"  md="2" lg="1">
+                    <v-row class=justify-center>
+                    <div class="caption grey--text">Время:</div>
+                    </v-row>
+                    <hr>
+                    <v-row class="justify-center">
+                    <v-col class="justify-center">
+                    <div class="pt-4 text-center">{{ lesson.time }}</div>
+                    </v-col>
+                    </v-row>
                   </v-col>
-                  <v-col cols="6" sm="4"  md="3">
-                    <div class="caption grey--text">Название:</div>
+                  <v-col cols="7" sm="5"  md="5" lg="6">
+                    <div class="caption grey--text">Название предмета:</div>
+                    <hr>
                     <div>{{ lesson.nameCh }}</div>
                     <hr>
                     <div>{{ lesson.nameZ }}</div>
                   </v-col>
-                  <v-col cols="6" sm="4" md="3">
+                  <v-col cols="8" sm="3" md="3" lg="2">
                     <div class="caption grey--text">Преподаватель:</div>
+                    <hr>
                     <div>{{ lesson.teacherCh }}</div>
                     <hr>
                     <div>{{ lesson.teacherZ }}</div>
                   </v-col>
-                  <v-col cols="6" sm="4" md="3">
-                    <div class="caption grey--text">Номер аудитории:</div>
+                  <v-col cols="4" sm="2" md="2" lg="2">
+                    <div class="caption grey--text">Аудитория:</div>
+                    <hr>
                     <div>{{ lesson.auditoryCh }}</div>
                     <hr>
                     <div>{{ lesson.auditoryZ }}</div>
--- src/views/Direction/Business-informatics/2_course/bi-181.vue
+++ src/views/Direction/Business-informatics/2_course/bi-181.vue
@@ -16,31 +16,59 @@ export default {
           lessons: [
             {
               number: "1",
-              time: "09:50-11:20",
-              nameCh: "ЖЦИС",
-              teacherCh: "Королёв О.Л.",
-              auditoryCh: '306',
-              link: "",
-              nameZ: 'ППИ',
+              time: "08:00-09:30",
+              nameCh: ' ',
+              teacherCh: ' ',
+              auditoryCh: '',
+              link: ' ',
+              nameZ: 'Проектирование пользовательского интерфейса',
               teacherZ: 'Королёв О.Л.',
-              auditoryZ: '307'
+              auditoryZ: '306',
             },
             {
-              number: "2",
-              time: "11:30-13:00",
-              name: "Культурология",
-              teacher: "Кравченко И.С.",
-              auditory: '306',
-              link: ""
+              number: '2',
+              time: '09:50-11:20',
+              nameCh: 'Безопасность жизнедеятельности',
+              teacherCh: 'Махаричева А.А.',
+              auditoryCh: '03',
+              link: ' ',
+              nameZ: 'Проектирование пользовательского интерфейса',
+              teacherZ: 'Королёв О.Л.',
+              auditoryZ: '306'
             },
             {
-              number: "3",
-              time: "13:20-14:50",
-              name: "БЖД",
-              teacher: "Предпопие",
-              auditory: '306',
-              link: ""
-            }
+              number: '3',
+              time: '11:30-13:00',
+              nameCh: 'Культурология',
+              teacherCh: 'Кравченко И.В.',
+              auditoryCh: '03',
+              link: ' ',
+              nameZ: 'Безопасность жизнедеятельности',
+              teacherZ: 'Скоромная Н.И.',
+              auditoryZ: '207'
+            },
+            {
+              number: '4',
+              time: '13:20-14:50',
+              nameCh: 'Культурология',
+              teacherCh: 'Кравченко И.В',
+              auditoryCh: '03',
+              link: ' ',
+              nameZ: ' ',
+              teacherZ: ' ',
+              auditoryZ: ' '
+            },
+            {
+              number: '5',
+              time: "15:00-16:30",
+              nameCh: ' ',
+              teacherCh: ' ',
+              auditoryCh: ' ',
+              link: ' ',
+              nameZ: ' ',
+              teacherZ: ' ',
+              auditoryZ: ' '
+            },
           ]
         },
 
